47104cc7d0cb9fa03eaf12a549be5bf28b14f443
Xuan-Vi Trinh-Nguyen <xuan-vi@MBP-de-Xuan-Vi.lan>
2020-05-21 17:46:33 -0400

    Set up button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
-
 import "components/Button.scss";
+const classNames = require('classnames')
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = classNames(
+      "button", 
+      {
+      " button--confirm": props.confirm,
+      " button--danger": props.danger
+      }
+   );
+   
+   return (
+      <button 
+         onClick={props.onClick} 
+         className={buttonClass}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
 }
