d23cfe573ca45a450a02549adeb567d8c84759b8
Xuan-Vi Trinh <xuanvi.trinhnguyen@gmail.com>
2020-06-10 15:44:30 -0400

    Add header, show and empty components to appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
     return (
-        <article className="appointment"></article>
+        <article className="appointment">
+            <Header time={props.time} />
+            {props.interview ? (
+                <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />
+            ) : (
+                <Empty />
+            )}
+        </article>
     )
 };
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment }from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -169,4 +169,10 @@ storiesOf("InterviewerListItem", module)
     ))
     .add("Edit", () => (
       <Form name="Baboo" interviewers={interviewers} interviewer={1} onSave={action("onSave")} onCancel={action("onCancel")}/>
+    ))
+    .add("Appointment Empty", () => (
+      <Fragment>
+        <Appointment id={1} time="12pm" interview={{ student: "Lydia Miller-Jones", interviewer }}/>
+        <Appointment id="last" time="1pm"/>
+      </Fragment>
     ))
